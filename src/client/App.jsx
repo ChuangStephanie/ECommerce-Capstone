281c6d268b2343e6041a3ec70e10c4a62f1fe4f7
Charles Graham <charlesgraham@MacBook-Pro.attlocal.net>
2023-09-16 15:27:45 -0700

    made basic Navbar

--- src/client/App.jsx
+++ src/client/App.jsx
@@ -1,17 +1,23 @@
-import { useState } from 'react';
-import reactLogo from './assets/react.svg';
-import Login from './components/Login';
+import { useState } from "react";
+import reactLogo from "./assets/react.svg";
+import { BrowserRouter, Routes, Route } from "react-router-dom";
+import Login from "./components/Login";
+import Nav from "./components/nav";
+import Home from "./components/Home";
 
 function App() {
   const [count, setCount] = useState(0);
 
   return (
-    <div className='App'>
-        <h1>Boilerplate</h1>
-        <img id='comp-img' src='./computer.png'></img>
-        <p>Replace the starter code in this template with something cool</p>
-        <Login />
-    </div>
+    <BrowserRouter>
+      <Nav />
+
+      
+      <Routes>
+        <Route path="/" element={<Home/>} />
+        <Route path="/login" element={<Login />} />
+      </Routes>
+    </BrowserRouter>
   );
 }
 
---
+++ src/client/components/Home.jsx
@@ -0,0 +1,13 @@
+import React from 'react'
+
+const Home = () => {
+  return (
+    <div className="App">
+        <h1>Boilerplate</h1>
+        <img id="comp-img" src="./computer.png"></img>
+        <p>Replace the starter code in this template with something cool</p>
+      </div>
+  )
+}
+
+export default Home
