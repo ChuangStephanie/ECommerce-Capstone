68ba954dc6d89a51927b89d6761914d5724cdd9c
ChuangStephanie <stephanie231951@yahoo.com>
2023-09-19 19:30:17 -0500

    added useEffect to home page

--- src/client/API/index.js
+++ src/client/API/index.js
@@ -1,10 +1,10 @@
-const baseURL = "https://localhost:5432/lizardsplushies";
+const baseURL = "https://localhost:3000";
 
 export async function fetchAllProducts() {
     try {
-        const response =  await fetch(`${baseURLI}/db/products`);
-        const productsAPI =  await response.json();
-        console.log("Fetched all products", productsAPI);
+        const response =  await fetch(`${baseURL}/api/products`);
+        const result =  await response.json();
+        console.log("Fetched all products", result.users);
     } catch (error) {
         console.log(error, "No products were fetched");
     }
--- src/client/components/Home.jsx
+++ src/client/components/Home.jsx
@@ -3,20 +3,22 @@ import { useNavigate } from "react-router-dom";
 import { fetchAllProducts } from "../API";
 
 export default function Home() {
-  const [posts, setPosts] = useState([]);
+  const [products, setProducts] = useState([]);
   const [error, setError] = useState(null);
   const [searchParam, setSearchParam] = useState("");
   const navigate = useNavigate();
 
   useEffect(() => {
     async function getAllProducts() {
-      const APIData = await fetchAllProducts();
-      if (APIData) {
-        setPosts(APIData.products);
+      const response = await fetchAllProducts();
+      if (response) {
+        setProducts(response);
+        console.log("Response:", response)
       } else {
-        setError(window.alert("No products found"));
+        setError(console.log("No products found"));
       }
     }
-  })
+    getAllProducts();
+  },[]);
 
 }
