5f3925fdbe57bd4a2ca5618468db946238c23a89
ChuangStephanie <stephanie231951@yahoo.com>
2023-09-19 20:01:19 -0500

    removed product from navbar

--- src/client/components/Nav.jsx
+++ src/client/components/Nav.jsx
@@ -1,24 +1,23 @@
-import React from 'react'
-import {Link} from 'react-router-dom'
+import React from "react";
+import { Link } from "react-router-dom";
 
 const Nav = () => {
   return (
     <nav>
-        <ul>
-            <li>
-                <Link to="/">Home</Link>
-            </li>
-            <li>
-                <Link to="/profile">Profile</Link>
-            </li>
-            <li>
-                <Link to="/products">Products</Link>
-            </li><li>
-                <Link to="/login">Login</Link>
-            </li>
-        </ul>
+      <ul>
+        <li>
+          <Link to="/">Home</Link>
+        </li>
+        <li>
+          <Link to="/profile">Profile</Link>
+        </li>
+        <li></li>
+        <li>
+          <Link to="/login">Login</Link>
+        </li>
+      </ul>
     </nav>
-  )
-}
+  );
+};
 
-export default Nav
+export default Nav;
