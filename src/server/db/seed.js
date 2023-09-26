0fc5886f7145931c46eb5ff4715c3d4b298d21e6
Charlesgraham66 <127766566+Charlesgraham66@users.noreply.github.com>
2023-09-26 16:57:20 -0700

    added users and products

--- src/server/db/seed.js
+++ src/server/db/seed.js
@@ -7,26 +7,67 @@ const users = [
     name: 'Emily Johnson',
     email: 'emily@example.com',
     password: 'securepass',
+    isAdmin: false,
   },
   {
     name: 'Liu Wei',
     email: 'liu@example.com',
     password: 'strongpass',
+    isAdmin: false,
   },
   {
     name: 'Isabella García',
     email: 'bella@example.com',
     password: 'pass1234',
+    isAdmin: false,
   },
   {
     name: 'Mohammed Ahmed',
     email: 'mohammed@example.com',
     password: 'mysecretpassword',
+    isAdmin: false,
   },
   {
     name: 'John Smith',
     email: 'john@example.com',
     password: 'password123',
+    isAdmin: false,
+  },
+  {
+    name: 'Jamie Chuang',
+    email: 'MxJChuang@money.com',
+    password: 'morbidcuriosity',
+    isAdmin: true,
+  },
+  {
+    name: 'Larry David',
+    email: 'Larry@example.com',
+    password: 'password987',
+    isAdmin: false,
+  },
+  {
+    name: 'David Goggins',
+    email: 'Goggins@example.com',
+    password: 'goggins123',
+    isAdmin: false,
+  },
+  {
+    name: 'Joe Rogan',
+    email: 'Rogan@example.com',
+    password: 'rogan456',
+    isAdmin: false,
+  },
+  {
+    name: 'Bijan Robinson',
+    email: 'Bijan@example.com',
+    password: 'bijan789',
+    isAdmin: false,
+  },
+  {
+    name: 'Manny Machado',
+    email: 'manny@example.com',
+    password: 'manny159',
+    isAdmin: false,
   },
 ]
 
@@ -55,6 +96,81 @@ const products = [
     name: 'Product 5',
     price: 49.99,
     description: 'Description for Product 5',
+  },
+  {
+    name: 'Product 6',
+    price: 4.99,
+    description: 'Description for product 6',
+  },
+  {
+    name: 'Product 7',
+    price: 14.99,
+    description: 'Description for product 7',
+  },
+  {
+    name: 'Product 8',
+    price: 24.99,
+    description: 'Description for product 8',
+  },
+  {
+    name: 'Product 9',
+    price: 34.99,
+    description: 'Description for product 9',
+  },
+  {
+    name: 'Product 10',
+    price: 44.99,
+    description: 'Description for product 10',
+  },
+  {
+    name: 'Product 11',
+    price: 9.99,
+    description: 'Description for product 11',
+  },
+  {
+    name: 'Product 12',
+    price: 4.99,
+    description: 'Description for product 12',
+  },
+  {
+    name: 'Product 13',
+    price: 14.99,
+    description: 'Description for product 13',
+  },
+  {
+    name: 'Product 14',
+    price: 12.99,
+    description: 'Description for product 14',
+  },
+  {
+    name: 'Product 15',
+    price: 34.99,
+    description: 'Description for product 15',
+  },
+  {
+    name: 'Product 16',
+    price: 3.99,
+    description: 'Description for product 16',
+  },
+  {
+    name: 'Product 17',
+    price: 15.99,
+    description: 'Description for product 17',
+  },
+  {
+    name: 'Product 18',
+    price: 29.99,
+    description: 'Description for product 18',
+  },
+  {
+    name: 'Product 19',
+    price: 9.99,
+    description: 'Description for product 19',
+  },
+  {
+    name: 'Product 20',
+    price: 19.99,
+    description: 'Description for product 20',
   },
 ]
 
