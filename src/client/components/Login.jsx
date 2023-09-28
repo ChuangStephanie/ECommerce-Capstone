d5a3a9726f724812d8cbd78c9ffc2812960b0961
Jarrett Allen <jrrttlln@gmail.com>
2023-09-28 17:10:54 -0500

    added toast to login.jsx

--- src/client/components/Login.jsx
+++ src/client/components/Login.jsx
@@ -1,11 +1,13 @@
 import React, { useContext, useState } from 'react';
 import { UserContext } from '../App';
-import { Link } from 'react-router-dom';
+import { Link, useNavigate } from 'react-router-dom';
+
 const Login = () => {
-  const {userLogged, setUserLogged} = useContext(UserContext)
+  const { userLogged, setUserLogged } = useContext(UserContext);
   const [email, setEmail] = useState('');
   const [password, setPassword] = useState('');
   const [message, setMessage] = useState('');
+  const [passwordError, setPasswordError] = useState(''); // New state for password error
 
   const handleEmailChange = (e) => {
     setEmail(e.target.value);
@@ -13,75 +15,82 @@ const Login = () => {
 
   const handlePasswordChange = (e) => {
     setPassword(e.target.value);
+    // Clear the password error message when the user starts typing a new password
+    setPasswordError('');
   };
 
-  const login = async() => {
+  const login = async () => {
     try {
-        const response = await fetch('http://localhost:3000/api/users/login', {
-            method: 'POST',
-            headers: {
-                'Content-Type' : 'application/json'
-            }, 
-            body: JSON.stringify({
-                email,
-                password
-            })
-        });
-        const result = await response.json();
-        setMessage(result.message);
-        if(!response.ok) {
-          throw(result)
-        }
-        setEmail('');
-        setPassword('');
-        setUserLogged(true)
+      // Check password length before sending the request
+      if (password.length < 6) {
+        setPasswordError('Password is too short (minimum 6 characters)');
+        return; // Don't proceed with the login if the password is too short
+      }
+
+      const response = await fetch('http://localhost:3000/api/users/login', {
+        method: 'POST',
+        headers: {
+          'Content-Type': 'application/json',
+        },
+        body: JSON.stringify({
+          email,
+          password,
+        }),
+      });
+
+      const result = await response.json();
+      setMessage(result.message);
+      if (!response.ok) {
+        throw result;
+      }
+      setEmail('');
+      setPassword('');
+      setUserLogged(true);
     } catch (err) {
-        console.error(`${err.name}: ${err.message}`);
+      console.error(`${err.name}: ${err.message}`);
     }
-  }
+  };
 
   const handleSubmit = (e) => {
     e.preventDefault();
     login();
-    const navigate = useNavigate();
-  navigate("/home")
   };
-
 
   return (
     <div>
       <h2>Login</h2>
       <form className="Login-form" onSubmit={handleSubmit}>
         <div>
-          <label htmlFor='email'></label>
+          <label htmlFor="email"></label>
           <input
-            placeholder='Email'
-            id='email'
+            placeholder="Email"
+            id="email"
             value={email}
             onChange={handleEmailChange}
             required
           />
         </div>
         <div>
-          <label htmlFor='password'></label>
+          <label htmlFor="password"></label>
           <input
-            placeholder='Password'
-            id='password'
+            placeholder="Password"
+            id="password"
             value={password}
             onChange={handlePasswordChange}
             required
+            type="password"
           />
+          {passwordError && <p className="error-message">{passwordError}</p>}
         </div>
-        <button className="login-btn" type='submit'>Login</button>
-        </form>
-        {/* <button onClick={() => navigate("/Register")}>Sign up here</button> */}
-        <Link className="register-link" to={"/register"}>
-          Don't have an account? Register here!
-        </Link>
-      
+        <button className="login-btn" type="submit">
+          Login
+        </button>
+      </form>
+      <Link className="register-link" to="/register">
+        Don't have an account? Register here!
+      </Link>
       <p>{message}</p>
     </div>
-
   );
 };
 
