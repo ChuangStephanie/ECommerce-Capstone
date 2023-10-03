f1acadfe751a003af058c5eaf633be81078ac886
Jarrett Allen <jrrttlln@gmail.com>
2023-10-03 17:07:22 -0500

    fixed home.jsx and page alignment in css

--- src/client/components/Home.jsx
+++ src/client/components/Home.jsx
@@ -1,12 +1,12 @@
 import React, { useEffect, useState } from "react";
-import { useNavigate, Link } from "react-router-dom"; // Import Link from react-router-dom
+import { useNavigate, Link } from "react-router-dom";
 import { fetchAllProducts } from "../API";
 
 export default function Home() {
   const [products, setProducts] = useState([]);
   const [error, setError] = useState(null);
   const [searchParams, setSearchParams] = useState("");
-  const [isLoading, setIsLoading] = useState(true); // Initialize loading state
+  const [isLoading, setIsLoading] = useState(true);
   const navigate = useNavigate();
 
   useEffect(() => {
@@ -15,14 +15,14 @@ export default function Home() {
         const response = await fetchAllProducts();
         if (response) {
           setProducts(response);
-          setIsLoading(false); // Set loading state to false when products are loaded
+          setIsLoading(false);
         } else {
-          setError("No products found");
-          setIsLoading(false); // Set loading state to false when there's an error
+          setError("Error loading products");
+          setIsLoading(false);
         }
       } catch (error) {
         setError("Error loading products");
-        setIsLoading(false); // Set loading state to false when there's an error
+        setIsLoading(false);
       }
     }
     getAllProducts();
@@ -56,25 +56,16 @@ export default function Home() {
             <p className="error-message">{error}</p>
           ) : (
             <div className="productscontainer">
-              {productsToDisplay.length === 0 ? (
-
-                <p>
-                  No products found. <a href="/">Go to Home</a>
-                </p>
-              ) : (
-                productsToDisplay.map((product) => (
-                  <div key={product.id} className="indivproduct">
-                    <h3>
-                      <Link to={`/${product.id}`}>{product.name}</Link>
-                    </h3>
-                    <p>{product.price}</p>
-                    {/* write if/else code here to control whether or not edit/delete buttons show up */}
-                    <button>Edit</button>
-                    <button>Delete</button>
-                    {/* buttons should only show for admin user */}
-                  </div>
-                ))
-              )}
+              {productsToDisplay.map((product) => (
+                <div key={product.id} className="indivproduct">
+                  <h3>
+                    <Link to={`/${product.id}`}>{product.name}</Link>
+                  </h3>
+                  <p>{product.price}</p>
+                  <button>Edit</button>
+                  <button>Delete</button>
+                </div>
+              ))}
             </div>
           )}
         </>
