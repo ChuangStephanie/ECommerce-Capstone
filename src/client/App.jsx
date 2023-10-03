90d5b110bfd9830bc7de2d8a2fa1b5b9caf370e9
Charlesgraham66 <127766566+Charlesgraham66@users.noreply.github.com>
2023-10-03 09:08:34 -0700

    changes to subitems and searchbar

--- src/client/App.jsx
+++ src/client/App.jsx
@@ -1,5 +1,5 @@
-import { useState } from "react";
-import { Routes, Route } from "react-router-dom";
+import { useEffect, useState } from "react";
+import { Routes, Route, useNavigate } from "react-router-dom";
 import Login from "./components/Login";
 import Nav from "./components/Nav";
 import Home from "./components/Home";
@@ -11,13 +11,46 @@ import Products from "./components/Products";
 import Keychains from "./components/Keychains";
 import OnSale from "./components/OnSale";
 import Plushies from "./components/Plushies";
+import { fetchAllProducts } from "./API";
 
 export const UserContext = createContext();
 function App() {
   const [userLogged, setUserLogged] = useState(false);
 
+  const [products, setProducts] = useState([]);
+  const [error, setError] = useState(null);
+  const [searchParams, setSearchParams] = useState("");
+  const [isLoading, setIsLoading] = useState(true); // Initialize loading state
+
+  async function getAllProducts() {
+    try {
+      const response = await fetchAllProducts();
+      if (response) {
+        setProducts(response);
+        setIsLoading(false); // Set loading state to false when products are loaded
+      } else {
+        setError("No products found");
+        setIsLoading(false); // Set loading state to false when there's an error
+      }
+    } catch (error) {
+      setError("Error loading products");
+      setIsLoading(false); // Set loading state to false when there's an error
+    }
+  }
+
+  useEffect(() => {
+    getAllProducts();
+  }, []);
+
+  const productsToDisplay = searchParams
+    ? products.filter((p) =>
+        p.name.toLowerCase().includes(searchParams.toLowerCase())
+      )
+    : [];
+
+
   return (
-    <UserContext.Provider value={{ userLogged, setUserLogged }}>
+    <UserContext.Provider value={{ userLogged, setUserLogged, products, setProducts, error, setError, searchParams, setSearchParams, isLoading, setIsLoading, getAllProducts, productsToDisplay }}>
       <Nav />
 
       <Routes>
--- src/client/components/Home.jsx
+++ src/client/components/Home.jsx
@@ -1,84 +1,17 @@
 import React, { useEffect, useState } from "react";
-import { useNavigate, Link } from "react-router-dom"; // Import Link from react-router-dom
-import { fetchAllProducts } from "../API";
+import { Link } from "react-router-dom"; // Import Link from react-router-dom
+
 
 export default function Home() {
-  const [products, setProducts] = useState([]);
-  const [error, setError] = useState(null);
-  const [searchParams, setSearchParams] = useState("");
-  const [isLoading, setIsLoading] = useState(true); // Initialize loading state
-  const navigate = useNavigate();
+  
 
-  useEffect(() => {
-    async function getAllProducts() {
-      try {
-        const response = await fetchAllProducts();
-        if (response) {
-          setProducts(response);
-          setIsLoading(false); // Set loading state to false when products are loaded
-        } else {
-          setError("No products found");
-          setIsLoading(false); // Set loading state to false when there's an error
-        }
-      } catch (error) {
-        setError("Error loading products");
-        setIsLoading(false); // Set loading state to false when there's an error
-      }
-    }
-    getAllProducts();
-  }, []);
-
-  const productsToDisplay = searchParams
-    ? products.filter((p) =>
-        p.name.toLowerCase().includes(searchParams.toLowerCase())
-      )
-    : [];
-
+  
   return (
     <>
       <h1 className="hometitle">Lizards Plushies</h1>
-      <div className="searchbar">
-        <label>
-          Search: {""}
-          <input
-            type="text"
-            placeholder="Search Products"
-            onChange={(e) => setSearchParams(e.target.value)}
-          />
-        </label>
-      </div>
+      
 
-      {isLoading ? (
-        <p>Loading...</p>
-      ) : (
-        <>
-          {error ? (
-            <p className="error-message">{error}</p>
-          ) : (
-            <div className="productscontainer">
-              {productsToDisplay.length === 0 ? (
-
-                <p>
-                  {/* No products found. <a href="/">Go to Home</a> */}
-                </p>
-              ) : (
-                productsToDisplay.map((product) => (
-                  <div key={product.id} className="indivproduct">
-                    <h3>
-                      <Link to={`/${product.id}`}>{product.name}</Link>
-                    </h3>
-                    <p>{product.price}</p>
-                    {/* write if/else code here to control whether or not edit/delete buttons show up */}
-                    <button>Edit</button>
-                    <button>Delete</button>
-                    {/* buttons should only show for admin user */}
-                  </div>
-                ))
-              )}
-            </div>
-          )}
-        </>
-      )}
+      
     </>
   );
 }
