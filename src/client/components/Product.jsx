ae538b320486f4a870838327f3ed5bc69d952b07
Charlesgraham66 <127766566+Charlesgraham66@users.noreply.github.com>
2023-10-10 15:52:36 -0700

    fixed sort, fixed sidebar and fixed home logo

--- src/client/components/Product.jsx
+++ src/client/components/Product.jsx
@@ -1,17 +1,71 @@
 import React from "react";
-import { Box, Link } from "@mui/material";
 import Ghost from "../assets/ghost.png";
+import { fetchAllProducts } from '../API';
+import { Link } from 'react-router-dom';
+import {
+  Card,
+  CardMedia,
+  CardContent,
+  CardActions,
+  Typography,
+  IconButton,
+} from '@mui/material';
+import AddShoppingCartIcon from '@mui/icons-material/AddShoppingCart';
+
+import { styled } from '@mui/system';
+
+const useStyles = styled('div')({
+  root: {
+    maxWidth: '100%',
+  },
+  media: {
+    height: 0,
+    paddingTop: '56.25%', // 16:9
+  },
+  cardActions: {
+    display: 'flex',
+    justifyContent: 'flex-end',
+  },
+  cardContent: {
+    display: 'flex',
+    justifyContent: 'space-between',
+  },
+});
 
 const Product = ({product}) => {
+    const classes = useStyles;
   return (
-    <Link href={`/products/${product.id}`} key={product.id}>
-      <Box className="productContent">
-        <img src={Ghost} alt={product.name} />
-        <h3>{product.name}</h3>
-        <p>{product.description}</p>
-        <p>{product.price}</p>
-      </Box>
-    </Link>
+    <Card key={product.id} className={classes.root}>
+              <Link to={`/products/${product.id}`}>
+                <CardMedia
+                  component="img"
+                  className={classes.media}
+                  image={Ghost}
+                  title={product.name}
+                />
+                <CardContent>
+                  <div className={classes.cardContent}>
+                    <Typography variant="h5" gutterBottom>
+                      {product.name}
+                    </Typography>
+                    <Typography variant="h5">{product.price}</Typography>
+                  </div>
+                  <Typography
+                    dangerouslySetInnerHTML={{ __html: product.description }}
+                    variant="body2"
+                    color="textSecondary"
+                  />
+                </CardContent>
+              </Link>
+              <CardActions disableSpacing className={classes.cardActions}>
+                <IconButton
+                  aria-label="Add to Cart"
+                  onClick={() => addToCart(product)}
+                >
+                  <AddShoppingCartIcon />
+                </IconButton>
+              </CardActions>
+            </Card>
   );
 };
 
