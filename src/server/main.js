534bef4accc27335781ffa17e2007c6552fa183a
ChuangStephanie <stephanie231951@yahoo.com>
2023-10-10 18:08:41 -0500

    reformatted and removed some comments

--- src/server/main.js
+++ src/server/main.js
@@ -1,60 +1,55 @@
-require('dotenv').config()
+require("dotenv").config();
 
-const stripe = require('stripe')('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');
-const express = require('express');
-const router = require('vite-express');
+const stripe = require("stripe")(
+  "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"
+);
+const express = require("express");
+const router = require("vite-express");
 const app = express();
 
-const bodyParser = require('body-parser')
+const bodyParser = require("body-parser");
 app.use(bodyParser.json());
 
-app.use(express.static('public'))
+app.use(express.static("public"));
 
-const db = require('./db/client')
+const db = require("./db/client");
 db.connect()
-.then(() => {
-  console.log('Database connected successfully');
-})
-.catch((error) => {
-  console.error('Error connecting to the database:', error);
-});
+  .then(() => {
+    console.log("Database connected successfully");
+  })
+  .catch((error) => {
+    console.error("Error connecting to the database:", error);
+  });
 
-const apiRouter = require('./api');
-app.use('/api', apiRouter);
+const apiRouter = require("./api");
+app.use("/api", apiRouter);
 
-const YOUR_DOMAIN = 'http://localhost:3000';
+const YOUR_DOMAIN = "http://localhost:3000";
 
-app.post('/create-checkout-session', async (req, res) => {
-  const { line_items } = req.body
+app.post("/create-checkout-session", async (req, res) => {
+  const { line_items } = req.body;
   console.log(line_items);
   const session = await stripe.checkout.sessions.create({
-    // line_items: [
-    //   {
-    //     // Provide the exact Price ID (for example, pr_1234) of the product you want to sell
-    //     price: 'price_1NzWUZLy2DFROTD9qmeVEB0C',
-    //     quantity: 1,
-    //   },
-    // ],
     line_items,
-    mode: 'payment',
+    mode: "payment",
     success_url: `${YOUR_DOMAIN}?success=true`,
     cancel_url: `${YOUR_DOMAIN}?canceled=true`,
   });
 
-  res.json({url: session.url});
+  res.json({ url: session.url });
 });
 
-var allowCrossDomain = function(req, res, next) {
-  res.header('Access-Control-Allow-Origin', "*");
-  res.header('Access-Control-Allow-Methods', 'GET,PUT,POST,DELETE');
-  res.header('Access-Control-Allow-Headers', 'Content-Type');
+var allowCrossDomain = function (req, res, next) {
+  res.header("Access-Control-Allow-Origin", "*");
+  res.header("Access-Control-Allow-Methods", "GET,PUT,POST,DELETE");
+  res.header("Access-Control-Allow-Headers", "Content-Type");
   next();
-}
+};
 
 app.use(allowCrossDomain);
 
 router.listen(app, 3000, () =>
-  console.log('Server is listening on port 3000...')
+  console.log("Server is listening on port 3000...")
 );
 
 module.exports = router;
