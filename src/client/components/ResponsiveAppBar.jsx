7d3c463a75d339c656fbc6026718aea17efa5c48
Charlesgraham66 <127766566+Charlesgraham66@users.noreply.github.com>
2023-10-11 19:00:09 -0700

    fixed margin from settings to cart in appbar

--- src/client/components/ResponsiveAppBar.jsx
+++ src/client/components/ResponsiveAppBar.jsx
@@ -157,7 +157,7 @@ function ResponsiveAppBar() {
 
           <Box sx={{ flexGrow: 0 }}>
             <Tooltip title="Open settings">
-              <IconButton onClick={handleOpenUserMenu} sx={{ p: 0 }}>
+              <IconButton style={{marginRight: '20px'}} onClick={handleOpenUserMenu} sx={{ p: 0 }}>
                 <Avatar alt="Remy Sharp" src="/static/images/avatar/2.jpg" />
               </IconButton>
             </Tooltip>
