be860076db8867d84042414d414a448bde633e1f
Jarrett Allen <jrrttlln@gmail.com>
2023-10-13 16:07:35 -0500

    added footer

--- src/client/App.jsx
+++ src/client/App.jsx
@@ -1,42 +1,43 @@
-import { useEffect, useState } from "react";
-import { Routes, Route, useNavigate } from "react-router-dom";
-import { ToastContainer } from 'react-toastify';  // Import ToastContainer
-import 'react-toastify/dist/ReactToastify.css';   // Import CSS for the toast notifications
-import Login from "./components/Login";
-import ResponsiveAppBar from "./components/ResponsiveAppBar";
-import Home from "./components/Home";
-import Register from "./components/Register";
-import Details from "./components/Details";
-import Cart from "./components/Cart"
-import { createContext } from "react";
-import Products from "./components/Products/";
-import Keychains from "./components/Keychains";
-import OnSale from "./components/OnSale";
-import Plushies from "./components/Plushies";
-import { fetchAllProducts } from "./API";
+import React, { useEffect, useState } from 'react';
+import { Routes, Route } from 'react-router-dom';
+import ResponsiveAppBar from './components/ResponsiveAppBar';
+import Footer from './components/Footer'; // Import the Footer component
+import { ToastContainer } from 'react-toastify';
+import 'react-toastify/dist/ReactToastify.css';
+import Login from './components/Login';
+import Home from './components/Home';
+import Register from './components/Register';
+import Details from './components/Details';
+import Cart from './components/Cart';
+import { createContext } from 'react';
+import Products from './components/Products/';
+import Keychains from './components/Keychains';
+import OnSale from './components/OnSale';
+import Plushies from './components/Plushies';
+import { fetchAllProducts } from './API';
 
 export const UserContext = createContext();
+
 function App() {
   const [userLogged, setUserLogged] = useState(false);
-
   const [products, setProducts] = useState([]);
   const [error, setError] = useState(null);
-  const [searchParams, setSearchParams] = useState("");
-  const [isLoading, setIsLoading] = useState(true); // Initialize loading state
+  const [searchParams, setSearchParams] = useState('');
+  const [isLoading, setIsLoading] = useState(true);
 
   async function getAllProducts() {
     try {
       const response = await fetchAllProducts();
       if (response) {
         setProducts(response);
-        setIsLoading(false); // Set loading state to false when products are loaded
+        setIsLoading(false);
       } else {
-        setError("No products found");
-        setIsLoading(false); // Set loading state to false when there's an error
+        setError('No products found');
+        setIsLoading(false);
       }
     } catch (error) {
-      setError("Error loading products");
-      setIsLoading(false); // Set loading state to false when there's an error
+      setError('Error loading products');
+      setIsLoading(false);
     }
   }
 
@@ -50,37 +51,40 @@ function App() {
       )
     : [];
 
-
   return (
     <>
-      <UserContext.Provider value={{ userLogged, setUserLogged, products, setProducts, error, setError, searchParams, setSearchParams, isLoading, setIsLoading, getAllProducts, productsToDisplay }}>
-      <ResponsiveAppBar />
-
-      <Routes>
-      <Route path="/" element={<Home />} />
-
-      <Route path="/login" element={<Login />} />
-
-      <Route path="/register" element={<Register />} />
-        
-      <Route path="/products/:id" element={<Details />} />
-      {/* <Route path="/profile" element={<Profile />} /> */}
-
-      <Route path="/products" element={<Products />} />
-
-      <Route path="/category/keychains" element={<Keychains />} />
-
-      <Route path="/category/on-sale-items" element={<OnSale />} />
-
-      <Route path="/category/plushies" element={<Plushies />} />
-
-      <Route path="/cart" element={<Cart />} />
-
-      </Routes>
-    </UserContext.Provider>
-    <ToastContainer position="top-right" autoClose={3000} hideProgressBar={false} />
-  </>
-    
+      <UserContext.Provider
+        value={{
+          userLogged,
+          setUserLogged,
+          products,
+          setProducts,
+          error,
+          setError,
+          searchParams,
+          setSearchParams,
+          isLoading,
+          setIsLoading,
+          getAllProducts,
+          productsToDisplay,
+        }}
+      >
+        <ResponsiveAppBar />
+        <Routes>
+          <Route path="/" element={<Home />} />
+          <Route path="/login" element={<Login />} />
+          <Route path="/register" element={<Register />} />
+          <Route path="/products/:id" element={<Details />} />
+          <Route path="/products" element={<Products />} />
+          <Route path="/category/keychains" element={<Keychains />} />
+          <Route path="/category/on-sale-items" element={<OnSale />} />
+          <Route path="/category/plushies" element={<Plushies />} />
+          <Route path="/cart" element={<Cart />} />
+        </Routes>
+        <Footer />
+        <ToastContainer position="top-right" autoClose={3000} hideProgressBar={false} />
+      </UserContext.Provider>
+    </>
   );
 }
 
