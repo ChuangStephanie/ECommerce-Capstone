f5e223409d315cf4dde143a656c8044e1ced454f
carlosalviranatal <carlosalviranatal@DESKTOP-A4A091C.localdomain>
2023-10-15 12:20:48 -0500

    test2

--- src/server/main.js
+++ src/server/main.js
@@ -42,7 +42,7 @@ app.post("/create-checkout-session", async (req, res) => {
 const allowCrossDomain = function (req, res, next) {
   res.header("Access-Control-Allow-Origin", "*");
   res.header("Access-Control-Allow-Methods", "GET,PUT,POST,DELETE");
-  res.header("Access-Control-Allow-Headers", "*");
+  res.header("Access-Control-Allow-Headers", "Origin, X-Requested-With, Content-Type, Accept");
   next();
 };
 
