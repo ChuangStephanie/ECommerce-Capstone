d619cb0425db8d393852d9907a34d1b67ac8cee9
ChuangStephanie <stephanie231951@yahoo.com>
2023-10-15 12:14:41 -0500

    test2

--- src/server/main.js
+++ src/server/main.js
@@ -42,7 +42,7 @@ app.post("/create-checkout-session", async (req, res) => {
 const allowCrossDomain = function (req, res, next) {
   res.header("Access-Control-Allow-Origin", "*");
   res.header("Access-Control-Allow-Methods", "GET,PUT,POST,DELETE");
-  res.header("Access-Control-Allow-Headers", "*");
+  res.header("Access-Control-Allow-Headers", "Content-Type");
   next();
 };
 
